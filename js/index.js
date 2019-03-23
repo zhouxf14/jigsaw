e15e622bd852280c4997f63adf2635af8139a87a
zhouxf14 <zhouxf14@163.com>
2019-03-23 18:59:04 -0400

    expert update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-  $(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy({
+    	scrollOffset: 70
+    });
+	$('.tabs').tabs();
+	$("#e1").click(function() {
+		$('#intro').hide();
+		$('#select').hide();
+		$('#expert1').show();
+	});
+	$("#e2").click(function() {
+		$('#intro').hide();
+		$('#select').hide();
+		$('#expert2').show();
+	});
+});
