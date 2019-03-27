c6446ed29f66ddde1e280b3340fd2c4f45fc06cc
zhouxf14 <zhouxf14@163.com>
2019-03-27 02:12:19 -0400

    update two quiz questions

--- js/index.js
+++ js/index.js
@@ -17,12 +17,22 @@ $(document).ready(function(){
 		$('#select').hide();
 		$('#expert2').show();
 	});
-	$("#expert1-quiz").click(function() {
+	$("#expert1-quiz-btn").click(function() {
 		$('#expert1 .quiz').show();
 		$(this).hide();
+		$("html, body").animate({
+			scrollTop: $("#expert1-quiz").offset().top }, {duration: 500,easing: "swing"});
+		return false;
 	});
-	$('#test').click(function() {
-		$("#ex1q1 .btn").attr('href','#modal1');
+
+	$('#ex1q1 p:nth-child(1)').click(function() {
+	// $('#test').click(function() {
+		$("#ex1q1 .fb-btn").attr('href','#modal1');
+	});
+
+	$('#ex1q2 p:nth-child(2)').click(function() {
+	// $('#test').click(function() {
+		$("#ex1q2 .fb-btn").attr('href','#modal1');
 	});
 
 });
